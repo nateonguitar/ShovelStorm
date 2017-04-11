6b2c4afdd1abf02f74d9dfbb105c549283979cc3
alexdawn76 <aylinzyanna@gmail.com>
2017-04-10 23:21:28 -0600

    CharacterMove Change

---
+++ Assets/CharacterMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    Animator CharacterToMove;
+
+    void Start()
+    {
+        CharacterToMove = GetComponent<Animator>();
+    }
+
+    void StartCharacterAnimator()
+    {
+        CharacterToMove.SetBool("IsStarted", true);
+    }
+
+}
